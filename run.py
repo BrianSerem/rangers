5346162f71e71277aa920328d0307a85f906969e
BrianSerem <brianseremiat@gmail.com>
2018-11-19 15:10:28 +0300

    [Feature 161890939] added commen.py file for modeling comment.

---
+++ run.py
@@ -0,0 +1,12 @@
+#from user import User
+#from comment import Comment
+
+comments = []
+users = []
+
+def run():
+	choice = input("Press a digit to begin using application\n 1. Login\n 2.Create a comment\n 3.Delete a comment")
+
+
+
+if choice == 1:
